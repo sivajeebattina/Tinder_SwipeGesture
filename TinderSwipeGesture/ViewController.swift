bebe2db227fd3f3a4362683188462a3312b965fe
Sivajee Battina <sivajee@zoojoo.be>
2017-08-14 14:53:58 +0530

    Initial Commit

---
+++ TinderSwipeGesture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderSwipeGesture
+//
+//  Created by Uber - Sivajee Battina on 14/08/17.
+//  Copyright © 2017 Uber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
